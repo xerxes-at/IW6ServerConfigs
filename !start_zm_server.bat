0fa2ba2fea1a7838f6efb907af84aec53ff6da6f
Chase Hall <c@chse.xyz>
2021-04-17 14:02:15 -0500

    split up bats/configs

--- !start_zm_server.bat
+++ !start_zm_server.bat
@@ -1,19 +1,26 @@
 @echo off
 ::///////////////////////////////////////////////////////////////////////
-::///  	         IW6x Server Configuration start-up file	      ///
+::///  	         IW6x Server Configuration start-up file	  		  ///
 ::///////////////////////////////////////////////////////////////////////
 ::// This config best view with Notepad++ or other non-windows notepad //
 ::///////////////////////////////////////////////////////////////////////
-::// 	Your edited server.cfg in the "iw6x" folder goes here  	       //
+::// 	Your edited server.cfg in the "main" folder goes here  	       //
 :://	This is were you edit your hostname, map & gamemodes from.     //
 ::///////////////////////////////////////////////////////////////////////
 
 
-set ServerFilename=server.cfg
+set ServerFilename=server_zm.cfg
+
+
+:://///////////////////////////////////////////////////////////////////////////
+::// 	What port do you want the server to run on?			  		         //
+:://	You must port forward this port & allow it through your firewall     //
+:://///////////////////////////////////////////////////////////////////////////
+set port=27016
 
 
 ::///////////////////////////////////////////////////////////////////////
 :://You're done!! WARNING!!! Don't mess with anything below this line  //
 ::///////////////////////////////////////////////////////////////////////
 
-start iw6x.exe -dedicated +exec %ServerFilename% +map_rotate
+start iw6x.exe -dedicated +set zombiesMode 1 +set net_port "%port%" +exec %ServerFilename% +map_rotate
